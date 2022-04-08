ad6e3d07841eae52dc485eb0c8bf5ab305949965
AbyPaul9 <akandeabayomipaul@gmail.com>
2022-04-08 07:54:12 -0700

    Single digit base numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point
+  *
+  * Return: 0
+  */
+int main(void)
+{
+	char i;
+
+	for (i = '0'; i <= '9'; ++i)
+	putchar(i);
+
+	putchar('\n');
+	return(0);
+}
