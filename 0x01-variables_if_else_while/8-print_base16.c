6c3e11a4b3f4f8dd3b4357d45695f4f9f424d24a
AbyPaul9 <akandeabayomipaul@gmail.com>
2022-04-08 08:42:23 -0700

    Print Hexadecimals

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point
+  *
+  *Return: 0
+  */
+int main(void)
+{
+	char x;
+
+	for (x = '0'; x <= '9'; ++x)
+	putchar(x);
+	for (x = 'a'; x <= 'f'; ++x)
+	putchar(x);
+
+	putchar('\n');
+
+	return (0);
+}
