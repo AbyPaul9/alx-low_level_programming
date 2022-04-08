fd5006fcdefa883d58cf0c3e24af25c2fb8a424d
AbyPaul9 <akandeabayomipaul@gmail.com>
2022-04-07 20:41:18 -0700

    Printing in C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
-#!/bin/bash
 #include <studio.h>
 /**
-* main - Entry point
-*
-* Return: Always 0 (Success)
-*/
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-    puts("\"Programming is like building a multillingual puzzle");
-    return (0);
+	puts("\"Programming is like building a multillingual puzzle");
+	return (0);
 }
