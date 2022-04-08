80a4d93328838ce92f89040c4a5f5849adda624a
AbyPaul9 <akandeabayomipaul@gmail.com>
2022-04-08 07:29:06 -0700

    Upper and lower cases

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,14 +7,15 @@
 int main(void)
 {
 	char c;
+
 	for (c = 'a'; c <= 'z'; c++)
 	{
 		putchar(c);
 	}
-	for(c = 'A'; c <= 'Z'; c++)
+	for (c = 'A'; c <= 'Z'; c++)
 	{
 		putchar(c);
 	}
-	putchar('\n');
+	putchar ('\n');
 	return(0);
 }
