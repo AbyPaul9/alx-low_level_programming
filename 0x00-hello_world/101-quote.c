7d4ec7c0ae30f37ae9bd4c95517733bc1a14b87a
AbyPaul9 <akandeabayomipaul@gmail.com>
2022-04-09 22:07:52 -0700

    Piece of art

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <uninstd.h>
+/**
+  * main - Entry point
+  *
+  * Return: 0
+  */
+int main(void)
+{
+	write(1, "and that piece of art is useful\" - Dora korpar, 2015-10-19\n", 59);
+	return(1);
+}	
