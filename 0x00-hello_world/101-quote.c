2322def84d5fe71ee1554d82c15d68f5c98b0c85
AbyPaul9 <akandeabayomipaul@gmail.com>
2022-04-09 22:50:14 -0700

    Piece of art

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,6 +7,6 @@
   */
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\" - Dora korpar, 2015-10-19\n", 59);
-	return (0);
+	fwrite(1, stderr,  "and that piece of art is useful\" - Dora korpar, 2015-10-19\n", 59);
+	return (1);
 }
