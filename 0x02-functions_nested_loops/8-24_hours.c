99fb8e498f636d65af5980e9b8ba348f8cede9bd
AbyPaul9 <akandeabayomipaul@gmail.com>
2022-04-12 09:05:24 -0700

    Printing every minutes of the day

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -3,20 +3,21 @@
 /**
   * jack_bauer(void)
   * Return: the sum of two numbers.
+  * @void - no argument
   */
 void jack_bauer(void)
 {
 	int h, m;
 
-	for (h = 0; m < 60; h++)
+	for (h = 0; m < 24; h++)
 	{
 		for (m = 0; m < 60; m++)
 		{
-			_putchar((h / 10) + '0');
-			_putchar((h % 10) + '0');
+			_putchar(h / 10 + '0');
+			_putchar(h % 10 + '0');
 			_putchar(':');
-			_putchar((m / 10) + '0');
-			_putchar((m % 10) + '0');
+			_putchar(m / 10 + '0');
+			_putchar(m % 10 + '0');
 			_putchar('\n');
 		}
 	}
