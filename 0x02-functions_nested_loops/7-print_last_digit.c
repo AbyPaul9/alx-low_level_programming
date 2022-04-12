3c020dcb4122053412c3228219fd5ccefee76901
AbyPaul9 <akandeabayomipaul@gmail.com>
2022-04-12 08:34:55 -0700

    Last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,8 +7,10 @@
   */
 int print_last_digit(int n)
 {
-	if (n < 0)
-		n = n * -1;
-	_putchar((n % 10) + '0');
-	return (n % 10);
+	int ln = n % 10;
+
+	if(n < 0)
+		ln = ln * -1;
+	_putchar(ln + '0');
+	return (ln);
 }
