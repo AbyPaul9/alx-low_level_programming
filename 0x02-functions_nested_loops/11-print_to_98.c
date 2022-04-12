cdb153afbcd087bec9e98287f9c6c24e3972e31a
AbyPaul9 <akandeabayomipaul@gmail.com>
2022-04-12 16:57:21 -0700

    Printing to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -18,38 +18,22 @@ void print_to_98(int n)
 
 		if (n < 98)
 
-				{
+		{
+			for (; n < 98; n++)
 
-							for (; n < 98; n++)
-
-										{
-
-														printf("%d, ", n);
-
-																}
-
-								}
-
-			else if (n > 98)
-
-					{
-
-								for (; n > 98; n--)
-
-											{
-
-															printf("%d, ", n);
-
-																	}
-
-									}
-
-				if (n == 98)
-
-						{
-
-									printf("%d\n", n);
-
-										}
-
+			{
+				printf("%d, ", n);
+			}
+		}
+		else if (n > 98)
+		{
+			for (; n > 98; n--)
+			{
+				printf("%d, ", n);
+			}
+		}
+		if (n == 98)
+		{
+			printf("%d\n", n);
+		}
 }
