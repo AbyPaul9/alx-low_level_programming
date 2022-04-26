32ce2dd2cbb85af9cd110282727dcbba7b7f3eff
AbyPaul9 <akandeabayomipaul@gmail.com>
2022-04-26 15:37:28 -0700

    Task 6

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -13,7 +13,7 @@ void print_chessboard(char (*a)[8])
 	for (i = 0; a[i][7]; i++)
 	{
 		for (j = 0; j < 8; j++)
-			putchar(a[i][j]);
-		putchar('\n');
+			_putchar(a[i][j]);
+		_putchar('\n');
 	}
 }
