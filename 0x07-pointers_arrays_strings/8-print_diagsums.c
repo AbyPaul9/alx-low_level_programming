d4e3b8cadd844b467aba354c1ae211c992b133d1
AbyPaul9 <akandeabayomipaul@gmail.com>
2022-04-26 15:51:10 -0700

    Task 7

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -18,11 +18,14 @@ void print_diagsums(int *a, int size)
 		s1 += a[i];
 		a += size;
 	}
+
 	a -= size;
+
 	for (i = 0; i < size; i++)
 	{
 		s2 += a[i];
 		a -= size;
 	}
+
 	printf("%d, %d\n", s1, s2);
 }
