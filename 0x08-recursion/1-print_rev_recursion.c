3f79a1e62e969ca6dfa43526746e720696661b25
AbyPaul9 <akandeabayomipaul@gmail.com>
2022-04-28 16:14:32 -0700

    Task 1

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -4,7 +4,6 @@
  * _print_rev_recursion - print a string
  * @s:string
  * Return:void
- * _putchar - print a char
  */
 
 void _print_rev_recursion(char *s)
@@ -12,6 +11,6 @@ void _print_rev_recursion(char *s)
 	if (*s)
 	{
 		_print_rev_recursion(s + 1);
-		putchar(*s);
+		_putchar(*s);
 	}
 }
