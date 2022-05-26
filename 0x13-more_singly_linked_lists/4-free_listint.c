1fc0bcf3e75f13c517c671bea3e0e6083a4fea08
AbyPaul9 <akandeabayomipaul@gmail.com>
2022-05-26 04:45:03 -0700

    Task 4

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * free_listint - free list
+ * @head:pointer to node element
+ * Return:void
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *check, *c;
+
+	c = head;
+	while (c != NULL)
+	{
+		check = c->next;
+		free(c);
+		c = check;
+	}
+}
