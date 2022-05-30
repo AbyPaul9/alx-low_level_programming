c4c9bf259d371992eea39b9cc954de5e278a2eca
AbyPaul9 <akandeabayomipaul@gmail.com>
2022-05-30 07:22:07 -0700

    Task 0

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,45 @@
+#include "main.h"
+/**
+ * _strlen - lenght of string
+ * @s:char
+ * Return:int
+ */
+int _strlen(const char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		continue;
+	}
+	return (i);
+}
+
+/**
+ * binary_to_uint - convert binary to unsigned int
+ * @b:string from 0 and 1 else NULL
+ *
+ * Return:unsigned int
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int k = 1;
+	unsigned int i = 0;
+	int c;
+	unsigned int len;
+
+	if (b == NULL)
+		return (0);
+	len = _strlen(b);
+	for (c = len - 1; c >= 0; c--)
+	{
+		if (b[c] != '0' && b[c] != '1')
+			return (0);
+		if (b[c] == '1')
+		{
+			i += k;
+		}
+		k *= 2;
+	}
+	return (i);
+}
