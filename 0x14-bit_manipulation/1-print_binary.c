22d6a055b28537d220a524fc8c7c0c8453fe68cb
AbyPaul9 <akandeabayomipaul@gmail.com>
2022-05-30 07:28:13 -0700

    Task 1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * print_binary - prints the binary
  * @n : unsigned int
@@ -6,7 +7,7 @@
 void print_binary(unsigned long int n)
 {
 	if (n > 1)
-		
+
 		print_binary(n >> 1);
 	_putchar((n & 1) + '0');
 }
