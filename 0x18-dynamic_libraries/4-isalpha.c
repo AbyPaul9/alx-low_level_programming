98510faab5e0a28dbd0c13383d3c01dc2ecf153d
AbyPaul9 <akandeabayomipaul@gmail.com>
2022-07-12 13:25:18 -0700

    Task 0

---
+++ 0x18-dynamic_libraries/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+  *_isalpha - tests whether a character is from english alphabet.
+  * @c: character to be checked.
+  * Return: 1 if the character is an english character.
+  * 0 if the character is not an english haracter.
+  */
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A'  && c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
